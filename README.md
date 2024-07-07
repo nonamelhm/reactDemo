e5f61ceb6630f2bdbbc179af5aad5eb2c14f263b
fplhm <2628764605@qq.com>
2024-07-07 13:11:22 +0800

    feat:页面右侧结构样式的调整

--- README.md
+++ README.md
@@ -605,6 +605,99 @@ export default Home;
   border-radius: 6px;
 }
 ```
+# 页面右侧结构样式的调整
+* 认识antd layout组件具体标签含义，根据UI调整
+* 右边部分v4版本利用`className = 'site-layout-background'`实现内容调整,v5版本不生效，自行利用了flex，这里举例我实现的v5版本
+* 实现——**面包屑位置调整** 、**右侧内容撑满调整**
+```tsx
+import React, {useState} from 'react';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import type {MenuProps} from 'antd';
+import {Breadcrumb, Layout, Menu, theme} from 'antd';
+
+const {Header, Content, Footer, Sider} = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    } as MenuItem;
+}
+
+const items: MenuItem[] = [
+    getItem('Option 1', '1', <PieChartOutlined/>),
+    getItem('Option 2', '2', <DesktopOutlined/>),
+    getItem('User', 'sub1', <UserOutlined/>, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined/>, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined/>),
+];
+
+const Home: React.FC = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: {colorBgContainer, borderRadiusLG},
+    } = theme.useToken();
+
+    return (
+        <Layout style={{minHeight: '100vh'}}>
+            {/*左侧侧边栏*/}
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                <div className="demo-logo-vertical"/>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
+            </Sider>
+            {/*右边内容*/}
+            <Layout>
+                {/*右边头部*/}
+                <Header style={{padding: 0, background: colorBgContainer}}>
+                    {/*调整面包屑位置到Header*/}
+                    <Breadcrumb style={{lineHeight: '64px', paddingLeft: "16px"}}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                </Header>
+                {/*右边内容——白色盒子*/}
+                <Content style={{
+                    margin: '16px 16px 0',
+                    flex: 1,
+                    height: '100%',
+                    padding: 24,
+                    minHeight: 360,
+                    background: colorBgContainer,
+                    borderRadius: borderRadiusLG,
+                }}>
+                    {/*窗口部分*/}
+
+                </Content>
+                {/*右边底部*/}
+                <Footer style={{padding: 0, textAlign: 'center', height: "48px", lineHeight: '48px'}}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default Home;
+```
 
 
 
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
     DesktopOutlined,
     FileOutlined,
@@ -6,10 +6,10 @@ import {
     TeamOutlined,
     UserOutlined,
 } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import type {MenuProps} from 'antd';
+import {Breadcrumb, Layout, Menu, theme} from 'antd';
 
-const { Header, Content, Footer, Sider } = Layout;
+const {Header, Content, Footer, Sider} = Layout;
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -28,48 +28,55 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Option 1', '1', <PieChartOutlined/>),
+    getItem('Option 2', '2', <DesktopOutlined/>),
+    getItem('User', 'sub1', <UserOutlined/>, [
         getItem('Tom', '3'),
         getItem('Bill', '4'),
         getItem('Alex', '5'),
     ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
+    getItem('Team', 'sub2', <TeamOutlined/>, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined/>),
 ];
 
 const Home: React.FC = () => {
     const [collapsed, setCollapsed] = useState(false);
     const {
-        token: { colorBgContainer, borderRadiusLG },
+        token: {colorBgContainer, borderRadiusLG},
     } = theme.useToken();
 
     return (
-        <Layout style={{ minHeight: '100vh' }}>
+        <Layout style={{minHeight: '100vh'}}>
+            {/*左侧侧边栏*/}
             <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-                <div className="demo-logo-vertical" />
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+                <div className="demo-logo-vertical"/>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
             </Sider>
+            {/*右边内容*/}
             <Layout>
-                <Header style={{ padding: 0, background: colorBgContainer }} />
-                <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
+                {/*右边头部*/}
+                <Header style={{padding: 0, background: colorBgContainer}}>
+                    {/*调整面包屑位置到Header*/}
+                    <Breadcrumb style={{lineHeight: '64px', paddingLeft: "16px"}}>
                         <Breadcrumb.Item>User</Breadcrumb.Item>
                         <Breadcrumb.Item>Bill</Breadcrumb.Item>
                     </Breadcrumb>
-                    <div
-                        style={{
-                            padding: 24,
-                            minHeight: 360,
-                            background: colorBgContainer,
-                            borderRadius: borderRadiusLG,
-                        }}
-                    >
-                        Bill is a cat.
-                    </div>
+                </Header>
+                {/*右边内容——白色盒子*/}
+                <Content style={{
+                    margin: '16px 16px 0',
+                    flex: 1,
+                    height: '100%',
+                    padding: 24,
+                    minHeight: 360,
+                    background: colorBgContainer,
+                    borderRadius: borderRadiusLG,
+                }}>
+                    {/*窗口部分*/}
+
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>
+                {/*右边底部*/}
+                <Footer style={{padding: 0, textAlign: 'center', height: "48px", lineHeight: '48px'}}>
                     Ant Design ©{new Date().getFullYear()} Created by Ant UED
                 </Footer>
             </Layout>
